440180a042d5f49512114d85e84873fd928222d0
JoseRinaldofilho <jose.vasconcelos@basis.com.br>
2021-09-27 13:59:37 -0300

    upload de  aquivos

---
+++ excluir_arquivo_e _mover_e_renomiar/index.php
@@ -0,0 +1,21 @@
+<?php
+// cria
+$testo = "jose filho";
+
+file_put_contents('nome.txt', $testo);
+
+///excluir
+
+//
+//unlink('nome.txt');
+//echo "Arquivo excluido com sucesso!!";
+
+//renomear
+//rename('nome.txt', 'teste.txt');
+//echo "arquivo auterado com sucesso !!";
+
+// mover um aquivo para pasta
+//rename('teste.txt','pasta/teste.txt');
+
+// copia um aquivo
+//copy('testo.txt', 'teste1.txt');
